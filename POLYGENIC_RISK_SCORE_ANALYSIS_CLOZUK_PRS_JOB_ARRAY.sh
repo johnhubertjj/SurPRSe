4817603338db57e9d30f4f00961fce7bb7a8dcc1
John Hubert <hubertjj@cardiff.ac.uk>
2017-01-08 16:51:58 +0000

    Changed the shell script for PRS so that it should work for arcca, will find out tonight after suicide squad

--- POLYGENIC_RISK_SCORE_ANALYSIS_CLOZUK_PRS_JOB_ARRAY.sh
+++ POLYGENIC_RISK_SCORE_ANALYSIS_CLOZUK_PRS_JOB_ARRAY.sh
@@ -4,11 +4,11 @@
 #PBS -P PR54
 #PBS -l select=1:ncpus=1:mpiprocs=1
 #PBS -l walltime 10:00:00
-#PBS -o PRS_CLOZUK_PGC_ANALYSIS.txt
-#PBS -e PRS_CLOZUK_combine_PGC_ANALYSIS_error.txt
+#PBS -o /home/c1020109/
+#PBS -e /home/c1020109/
 #PBS -j oe
-#PBS -J 1-22
-#PBS -r y
+#PBS -J 21-22:2
+#PBS -N c1020109_job_array
 
 cd $PBS_O_WORKDIR
 
@@ -18,7 +18,7 @@ module load plink/1.9a
 tar -zxvf CLOZUK_GWAS_BGE_chr${PBS_ARRAY_INDEX}.tar.gz
 gzip -dc PGC_table${PBS_ARRAY_INDEX}.txt.gz
 
-R CMD BATCH Clumping_CLOZUK_PGC.R 
+R CMD BATCH Clumping_CLOZUK_PGC.R ./extrainfo/Rout_files/CLOZUK_PGC_clumpinginfo_chr${PBS_ARRAY_INDEX}.Rout 
  
 plink 
  --bfile CLOZUK_GWAS_BGE_chr${PBS_ARRAY_INDEX}
@@ -35,7 +35,16 @@ tr -s ' ' '\t' < FT4.training_bge_LOC_removed_A.T_C.G.target_r0.2_1000kb.clumped
 cut -f 2,4,5,6 < CLOZUK_PGC_CLUMPED_chr${PBS_ARRAY_INDEX}.txt > CLOZUK_PGC_CLUMPED_FINAL_chr${PBS_ARRAY_INDEX}.txt
 rm CLOZUK_PGC_CLUMPED_chr${PBS_ARRAY_INDEX}.txt
 
-R CMD BATCH PRS_scoring_R_script.R
+R CMD BATCH PRS_scoring_R_script.R ./extrainfo/Rout_files/PRS_SCORING_INFO_CLOZUK_PGC_chr${PBS_ARRAY_INDEX}.Rout
+
+significant=(1e-4 1e-3 1e-2 0.05 0.1 0.2 0.3 0.4 0.5)
+
+for i in `seq 1 ${#significant[@]}` ;
+do
+	plink --silent --bfile CLOZUK_GWAS_BGE_chr${PBS_ARRAY_INDEX} --exclude extracted_Duplicate_snps_chr${PBS_ARRAY_INDEX}.txt --score scoring_PGC_CLOZUK_chromosome_${PBS_ARRAY_INDEX}_${significant[i]}.score --out profiles/chr_${PBS_ARRAY_INDEX}_clump_r0.2_1000kb_${significant[i]}
+
+done
+
 
 
 
