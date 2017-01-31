e82c78100b6312621df30907f77d67e1f917bcbd
John Hubert <hubertjj@cardiff.ac.uk>
2017-01-31 14:09:57 +0000

    updated the summary scripts to make it slightly more understandable and also included an updated CLOZUK_PGC combination R script which was altered but saved in the wrong area

--- Taking_in_summary_stat_data_for_chromosome_conversion.R
+++ Taking_in_summary_stat_data_for_chromosome_conversion.R
@@ -1,3 +1,5 @@
+### Setting up script for use both on server and on local
+
 System_information <- Sys.info()
 whereami <- System_information['user']
 
@@ -29,6 +31,13 @@ setwd(".")
 PGC_data <- fread(Filename)
 setkey(PGC_data,CHR)
 
+# separating out chromosome into certain sections
+# ideally for future, one of the shell script arguments will be the number of segmentations you wish to separate the chromosome into (maybe even taking from the PBS options)
+
+# below is aka:
+  # after setting the key to the chromosome number,
+  # take a subset of the data matching the chromsome number
+  # and write the subsetted table to a new table for each chromosome. 
 for (chromosome.number in 1:22) { 
 write.table(eval(parse(text = paste0("PGC_data[J(",chromosome.number,")]"))), file = paste0("PGC_table",chromosome.number,".txt"), quote = F, row.names = F)
 }
