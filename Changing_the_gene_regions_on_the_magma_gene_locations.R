d16ec14d4cc7ba300be0fe72535f81bdda79c097
John Hubert <hubertjj@cardiff.ac.uk>
2017-02-08 09:42:38 +0000

    Reorganised the repository so that extra scripts and files are in logical directories while the main script for the pipeline are in the main directory

--- Changing_the_gene_regions_on_the_magma_gene_locations.R
+++
@@ -1,6 +0,0 @@
-MAGMA_different_gene_regions <-fread("NCBI37.3.gene.loc",colClasses = c("numeric","character",rep("numeric",2),rep("character",2)))
-colnames(MAGMA_different_gene_regions) <- c("ID","Chromosome","BP_start","BP_finish","strand","Gene_symbol")
-MAGMA_different_gene_regions$BP_start <- MAGMA_different_gene_regions$BP_start - 35000
-MAGMA_different_gene_regions$BP_finish <- MAGMA_different_gene_regions$BP_finish + 10000
-
-write.table(MAGMA_different_gene_regions, file = "NCBI37_-35kb_+10kb.gene.loc",quote = F, col.names = F, row.names = F)
