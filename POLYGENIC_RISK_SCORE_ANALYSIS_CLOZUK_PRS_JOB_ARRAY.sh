84fcb090620b111b472982a6780e70a2b38c83a5
John Hubert <hubertjj@cardiff.ac.uk>
2017-02-21 17:31:02 +0000

    Started with adding versatility to each of the R scripts so that it can handle both batch processing and Serial processing when it comes to the analysis, this will come in handy down the line
    Former-commit-id: 6ab9651e184e92e6f906307554a62391379bcd41

--- POLYGENIC_RISK_SCORE_ANALYSIS_CLOZUK_PRS_JOB_ARRAY.sh
+++ POLYGENIC_RISK_SCORE_ANALYSIS_CLOZUK_PRS_JOB_ARRAY.sh
@@ -58,8 +58,8 @@ R CMD BATCH ${path_to_scripts}CLOZUK_PGC_COMBINE_final.R ./extrainfo/CLOZUK_PGC_
 # using plink to change the names to a CHR.POS identifier and remaking the files
 plink --bfile CLOZUK_GWAS_BGE_chr${chromosome_number} --update-name CLOZUK_chr${chromosome_number}_chr.pos.txt --make-bed --out ./output/CLOZUK_GWAS_BGE_chr${chromosome_number}_2
 
-#re-package the original files
-#tar -czvf CLOZUK_GWAS_BGE_chr${chromosome_number}.tar.gz CLOZUK_GWAS_BGE_chr${chromosome_number}.bed CLOZUK_GWAS_BGE_chr${chromosome_number}.bim CLOZUK_GWAS_BGE_chr${chromosome_number}.fam
+# re-package the original files
+# tar -czvf CLOZUK_GWAS_BGE_chr${chromosome_number}.tar.gz CLOZUK_GWAS_BGE_chr${chromosome_number}.bed CLOZUK_GWAS_BGE_chr${chromosome_number}.bim CLOZUK_GWAS_BGE_chr${chromosome_number}.fam
 
 # create files containing duplicate SNPs
 R CMD BATCH ${path_to_scripts}Clumping_CLOZUK_PGC.R ./extrainfo/CLOZUK_PGC_clumpinginfo_chr${chromosome_number}.Rout 
@@ -82,50 +82,6 @@ printf "%s\n\n" "$(tail -n +2 ./output/CLUMPED_EXTRACT_CLOZUK_chr${chromosome_nu
 
 # Create clumped plink files
 plink --bfile ./output/CLOZUK_GWAS_BGE_chr${chromosome_number}_2 --extract ./output/CLUMPED_EXTRACT_CLOZUK_chr${chromosome_number}.txt --make-bed --out ./output/CLOZUK_GWAS_BGE_CLUMPED_chr${chromosome_number}
-
-# recode genotypes for input into Python
-plink --bfile ./output/CLOZUK_GWAS_BGE_CLUMPED_chr${chromosome_number} --recode A --out ./output/CLOZUK_GWAS_BGE_CLUMPED_chr${chromosome_number}
-plink --bfile ./output/CLOZUK_GWAS_BGE_CLUMPED_chr${chromosome_number} --freq --out ./output/CLOZUK_GWAS_BGE_CLUMPED_chr${chromosome_number}
-
-# Get the MAF from CLOZUK and import into the PGC_summary_stats for PRS analysis
-awk '{ $6=$7=$8=$10=$12=$13=$14=$15=$16=$17=""; print$0}' ./output/PGC_table${chromosome_number}_new.txt > ./output/PGC_table_for_python${chromosome_number}.txt
-
-# match the SNP rows to the MAF rows using R script
-R CMD BATCH ${path_to_scripts}Prepare_both_CLOZUK_AND_PGC_for_PRS_MAF_Genotype.R ./extrainfo/Prepare_both_CLOZUK_PGC_MAF_chr${chromosome_number}.Rout
-
-# Final removal of headings for PRS analysis
-awk 'NR>1' ./output/CLOZUK_GWAS_BGE_CLUMPED_chr${chromosome_number}.raw > ./output/CLOZUK_GWAS_BGE_CLUMPED_chr${chromosome_number}_no_head.raw 
-
-# Create Final PRS outputs
-if [ ! -d "./output/PRS_scoring" ]; then
-   mkdir ./output/PRS_scoring
-fi
-
-# Needs the MAGMA script
-sh ${path_to_scripts}PRS_with_magma.sh ${chromosome_number} 
-
-# Run preparation for annotation file for python scripts
-R CMD BATCH ${path_to_scripts}MAGMA_python_annotation_table_creator.R ./extrainfo/MAGMA_annotation_table_creator.Rout
-
-# Run PRS python script # Loop through significance thresholds
-# Change for Raven or Local
-if [ "$whereami" == "raven13" ]; then
-   python ${path_to_scripts}PRS_scoring_parallel_clump_maf_JJ.py
-else
-   python ${path_to_scripts}PRS_scoring_parallel_clump_maf_JJ.py 
-fi
-
-#### alter below ####
-# Calculate the PRS and profiles
-# R CMD BATCH PRS_scoring_R_script.R ./extrainfo/PRS_SCORING_INFO_CLOZUK_PGC_chr${chromosome_number}.Rout
-
-#significant=(0.0001 0.001 0.01 0.05 0.1 0.2 0.3 0.4 0.5)
-
-#for i in `seq 1 ${#significant[@]}` ;
-#do
-#	plink --silent --bfile CLOZUK_GWAS_BGE_chr${chromosome_number} --exclude extracted_Duplicate_snps_chr${chromosome_number}.txt --score score/scoring_PGC_CLOZUK_chromosome_${chromosome_number}_${significant[i]}.score --out profiles/chr_${chromosome_number}_clump_r0.2_1000kb_${significant[i]}
-	
-#done
 
 
 
