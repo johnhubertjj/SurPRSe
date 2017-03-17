5cb5b18957dfcf971ce97ba4c438fa9f8c4314b9
John Hubert <hubertjj@cardiff.ac.uk>
2017-03-17 16:33:42 +0000

    Started streamlinining the PRS analysis so that the pipeline is beginning to become more versatile as well as run properly

--- PRS_ANALYSIS_CLOZUK_PRS_SERIAL.sh
+++ PRS_ANALYSIS_CLOZUK_PRS_SERIAL.sh
@@ -10,22 +10,38 @@
 #PBS -J 21-22:2
 #PBS -N c1020109_job_array_whole_genome
 
-
 # Run locally or on ARCCA
 whereami=$(uname -n)
 echo "$whereami"
 if [[ "$whereami" == *"raven"* ]]; then
   cd $PBS_O_WORKDIR
-  path_to_scripts="~/c1020109/PhD_scripts/Schizophrenia_PRS_pipeline_scripts/"
-  path_to_PGC_conversion="~/c1020109/PhD_scripts/Schizophrenia_PRS_pipeline_scripts/Summary_stat_manipulation"
-  path_to_CLOZUK_conversion="~/c1020109/PhD_scripts/Schizophrenia_PRS_pipeline_scripts/Genotype_dataset_manipulation"
+  path_to_scripts="~/$USER/PhD_scripts/Schizophrenia_PRS_pipeline_scripts/"
+  path_to_PGC_conversion="~/$USER/PhD_scripts/Schizophrenia_PRS_pipeline_scripts/Summary_stat_manipulation/"
+  path_to_CLOZUK_conversion="~/$USER/PhD_scripts/Schizophrenia_PRS_pipeline_scripts/Genotype_dataset_manipulation/"
+  path_to_MAGMA_scripts="~/$USER/PhD_scripts/Schizophrenia_PRS_pipeline_scripts/MAGMA/"
   number_of_files=($(find -E . -type f -regex '^./output/CLOZUK_GWAS_BGE_CLUMPED_chr[0-9]+.bed' -exec basename {} \;))	
+  
+  # assign arguments here for now because there are so many
+  training_set_usually_summary="PGC_table"
+  validation_set_usually_genotype="CLOZUK_GWAS_BGE_chr"
+  training_set_name="PGC"
+  validation_set_name="CLOZUK"
+  MAF_summary="FALSE"
+  MAF_threshold=0.01
+  MAF_genotype="TRUE"
+  INFO_summary="TRUE"
+  INFO_threshold=0.6	
+  Chromosomes_to_analyse=(`seq 1 22`)
+  Multiple_Training_set_tables="TRUE"
+  Running_in_Serial="TRUE"
+  sig_thresholds=(0.0001 0.001 0.01 0.05 0.1 0.2 0.3 0.4 0.5)
+
 # Load both Plink and R
   module purge
   module load R/3.3.0
   module load plink/1.9c3
   module load python/2.7.9-mpi
-
+  module load magma...something
 
 elif [ "$whereami" == 'v1711-0ab8c3db.mobile.cf.ac.uk' ]; then
   cd ~/Documents/testing_PRS_chromosome_22/whole_genome_testing/
@@ -42,8 +58,8 @@ echo "${number_of_files[@]}"
 length_of_array=`echo "${#number_of_files[@]}"`
 
 # Check for make_list_file
-if [ -f ./output/CLOZUK_PGC_FULL_GENOME_MAKE_LIST_INPUT.txt ]; then
-  rm ./output/CLOZUK_PGC_FULL_GENOME_MAKE_LIST_INPUT.txt
+if [ -f ./output/${validation_set_name}_${training_set_name}_FULL_GENOME_MAKE_LIST_INPUT.txt ]; then
+  rm ./output/${validation_set_name}_${training_set_name}_FULL_GENOME_MAKE_LIST_INPUT.txt
 fi
 
 # Create Make-list file by repeating the name of each file on each line of the make-list text document
@@ -54,41 +70,51 @@ length_of_array=`echo "$((${length_of_array} - ${num1}))"`
 for chromosome_number in `seq 0 ${length_of_array}` ;
 do
 current_file=$(basename ${number_of_files[$chromosome_number]} .bed)
-printf "${current_file}\n%.0s" {1} >> ./output/CLOZUK_PGC_FULL_GENOME_MAKE_LIST_INPUT.txt
+printf "${current_file}\n%.0s" {1} >> ./output/${validation_set_name}_${training_set_name}_FULL_GENOME_MAKE_LIST_INPUT.txt
 done
 
-echo "Press CTRL+C to proceed."
-trap "pkill -f 'sleep 1h'" INT
-trap "set +x ; sleep 1h ; set -x" DEBUG
+# echo "Press CTRL+C to proceed."
+# trap "pkill -f 'sleep 1h'" INT
+# trap "set +x ; sleep 1h ; set -x" DEBUG
 
 # merge all the clumped chromosomes together
 cd ./output/
-plink --merge-list ./CLOZUK_PGC_FULL_GENOME_MAKE_LIST_INPUT.txt --make-bed --out ./CLOZUK_PGC_FULL_GENOME
+plink --merge-list ./${validation_set_name}_${training_set_name}_FULL_GENOME_MAKE_LIST_INPUT.txt --make-bed --out ./${validation_set_name}_${training_set_name}_FULL_GENOME
 cd ..
 
 # merge all the PGC SNPs together into one table
-R CMD BATCH ${path_to_PGC_conversion}combining_summary_stats_tables_after_conversion_to_CHR_POS.R ./extrainfo/PGC_conversion.Rout 
+Rscript ${path_to_scripts}RscriptEcho.R ${path_to_PGC_conversion}combining_summary_stats_tables_after_conversion_to_CHR_POS.R ./extrainfo/${training_set_name}_conversion.Rout ${training_set_usually_name} ${Multiple_Training_set_tables} ${Chromosomes_to_analyse}  
 
 # recode genotypes for input into Python
-plink --bfile ./output/CLOZUK_PGC_FULL_GENOME --recode A --out ./output/CLOZUK_PGC_FULL_GENOME
-plink --bfile ./output/CLOZUK_PGC_FULL_GENOME --freq --out ./output/CLOZUK_PGC_FULL_GENOME
+plink --bfile ./output/${validation_set_name}_${training_set_name}_FULL_GENOME --recode A --out ./output/${validation_set_name}_${training_set_name}_FULL_GENOME
+plink --bfile ./output/${validation_set_name}_${training_set_name}_FULL_GENOME --freq --out ./output/${validation_set_name}_${training_set_name}_FULL_GENOME
 
 # Get the MAF from CLOZUK and import into the PGC_summary_stats for PRS analysis
-awk '{ $6=$7=$8=$10=$12=$13=$14=$15=$16=$17=""; print$0 }' ./output/combined_PGC_table_with_CHR.POS_identifiers.txt > ./output/PGC_table_for_python.txt
+awk '{ $6=$7=$8=$10=$12=$13=$14=$15=$16=$17=""; print$0 }' ./output/combined_${training_set_name}_table_with_CHR.POS_identifiers.txt > ./output/${training_set_name}_table_for_python.txt
 
 # match the SNP rows to the MAF rows using R script
-R CMD BATCH 'Serial' ${path_to_scripts}Prepare_both_CLOZUK_AND_PGC_for_PRS_MAF_Genotype.R ./extrainfo/Prepare_both_CLOZUK_PGC_MAF_chr.Rout
+Rscript ${path_to_scripts}RscriptEcho.R ${path_to_scripts}Prepare_both_${validation_set_name}_AND_${training_set_name}_for_PRS_MAF_Genotype.R ./extrainfo/Prepare_both_${validation_set_name}_${training_set_name}_MAF_chr.Rout ${Running_in_Serial} ${training_set_name} ${validation_set_name}
 
 # Final removal of headings for PRS analysis
-awk 'NR>1' ./output/CLOZUK_PGC_FULL_GENOME.raw > ./output/CLOZUK_PGC_FULL_GENOME_no_head.raw
+awk 'NR>1' ./output/${validation_set_name}_${training_set_name}_FULL_GENOME.raw > ./output/${validation_set_name}_${training_set_name}_FULL_GENOME_no_head.raw
 
-# Create Final PRS outputs
+# Create Final PRS outputs directory
 if [ ! -d "./output/PRS_scoring" ]; then
   	mkdir ./output/PRS_scoring
 fi
 
-# Needs the MAGMA script
-sh ${path_to_scripts}PRS_with_magma.sh ${chromosome_number}
+#########################
+#### MAGMA ANALYSIS #####
+#########################
+if [ ! -d "./extrainfo/MAGMA_analysis" ]; then
+	mkdir ./extrainfo/MAGMA_analysis
+fi
+### WHERE I GOT TO ####
+Rscript ${path_to_scripts}RscriptEcho.R ${path_to_scripts}Creation_of_filter_file_for_magma_gene.R ./extrainfo/Creation_of_filter_file_for_magma_gene.R ${training_set_name} ${validation_set_name} ${sig_thresholds[@]}
+
+
+# Run with MAGMA
+sh ${path_to_scripts}PRS_with_magma.sh ${sig_thresholds[@]} ${validation_set_name} ${training_set_name}
 
 # Run preparation for annotation file for python scripts
 R CMD BATCH ${path_to_scripts}MAGMA_python_annotation_table_creator.R ./extrainfo/MAGMA_annotation_table_creator.Rout
--- Prepare_both_CLOZUK_AND_PGC_for_PRS_MAF_Genotype.R
+++ Prepare_both_CLOZUK_AND_PGC_for_PRS_MAF_Genotype.R
@@ -1,12 +1,17 @@
 ### Start Timer
 ptm <- proc.time()
 
-##################################################
-# Checking location for serial or batch analysis #
-##################################################
+########################################
+# Adding in arguments from BASH script #
+########################################
 args <- commandArgs(trailingOnly = T)
-System_information <- Sys.info()
-whereami <- System_information['user']
+getwd()
+print(args)
+
+# specify the different input tables #
+in_serial <- args[3]
+Training_name <- args[4]
+Validation_name <- args[5]
 
 # Set up library
 library("data.table")
@@ -14,87 +19,36 @@ library("data.table")
 # Set working directory
 setwd(".")
 
-###############################
-## Location Finding Function ##
-###############################
 
-Batch.assignment <- function(wherami) {
-  if (whereami == "johnhubert") {
-    assign("chromosome.number", 22, envir = .GlobalEnv)
-    
-  } else if (whereami == 'JJ') {
-    assign("chromosome.number", 22, envir = .GlobalEnv)
-    
-  } else if (whereami == "c1020109") {
-    
-    # Preparing to run in Job array
-    AI <- Sys.getenv("PBS_ARRAY_INDEX")
-    AI <- as.numeric(AI)
-    assign("chromosome.number", AI, envir = .GlobalEnv)
-    
-  } else {
-    stop("current environment NOT at work/home or on servers, please add to script above to specify where you are and what type of analysis you want to do")
-  }
-}
-
-# Find out whether you want to run the script in a batch format or in serial
-if( args[1] == 'Batch') {
-  
-  # Assign chromosome number as job array variable
-  Batch.assignment(whereami)
-  
-  # Read in MAF data
-  MAF_clumped_CLOZUK_data <- fread(paste0("./output/CLOZUK_GWAS_BGE_CLUMPED_chr",chromosome.number,".frq"))
-  PGC_useful_columns <- fread(paste0("./output/PGC_table_for_python",chromosome.number,".txt"))
-  
-  # use data table structure to merge tables together and add MAF while removing useless columns
-  PGC_CLOZUK_python_table <- merge(MAF_clumped_CLOZUK_data,PGC_useful_columns, by = c('CHR','SNP',"A1","A2"), all.x = T, all.y = F)
-  PGC_CLOZUK_python_table[, NCHROBS := NULL][,.(CHR,SNP,BP,A1,A2,BETA,P,MAF)]
-  ###INSERT CONTROL SO THAT NO EXTRA COLUMNS ARE ADDED (as you've specified that you want to include all x columns)
-  
-  #Read in .raw file
-  RAW.CLOZUK.CLUMPED <- fread(paste0("./output/CLOZUK_GWAS_BGE_CLUMPED_chr",chromosome.number,".raw"))
-  
-  # Flow control for assigning Row ID
-  testing_col_names <- paste0(PGC_CLOZUK_python_table$SNP,"_",PGC_CLOZUK_python_table$A1)
-  testing_col_names_raw <- colnames(RAW.CLOZUK.CLUMPED)
-  
-  if(all(testing_col_names == testing_col_names_raw[7:length(testing_col_names_raw)]) != T){
-    stop("ROW.ID from input data does not match genotype data, check the column names of both .raw file and the merged file")
-  }else{
-    PGC_CLOZUK_python_table[, ROW.ID := c(testing_col_names)]
-  }
-  # rearrange order to match Python script downstream
-  setcolorder(PGC_CLOZUK_python_table, c(9,1,2,6,3,4,7,8,5))
-  
-  # Write MAF file to working_directory
-  write.table(PGC_CLOZUK_python_table, file = paste0("./output/CLOZUK_GWAS_BGE_CLUMPED_PGC_MAF_FINAL",chromosome.number,".txt"),quote = F, row.names = T, col.names = F)
-  
-} else if (args[1] == 'Serial') {
+if (in_serial == 'TRUE') {
   
 # Read in MAF data
-MAF_clumped_CLOZUK_data <- fread("./output/CLOZUK_PGC_FULL_GENOME.frq")
+MAF_clumped_CLOZUK_data <- fread(paste0("./output/",Validation_name,"_",Training_name,"_FULL_GENOME.frq"))
 vector_for_place_of_SNP <- c(1:length(MAF_clumped_CLOZUK_data$CHR))
 MAF_clumped_CLOZUK_data[,place.names := vector_for_place_of_SNP]
-PGC_useful_columns <- fread("./output/PGC_table_for_python.txt")
+PGC_useful_columns <- fread(paste0("./output/",Training_name,"_table_for_python.txt"))
 
 # use data table structure to merge tables together and add MAF while removing useless columns
 PGC_CLOZUK_python_table <- merge(MAF_clumped_CLOZUK_data,PGC_useful_columns, by = c('CHR','SNP',"A1","A2"), all.x = T, all.y = F)
-PGC_CLOZUK_python_table[, NCHROBS := NULL][,.(CHR,SNP,BP,A1,A2,BETA,P,MAF,place.names)]
+
+PGC_CLOZUK_python_table <- PGC_CLOZUK_python_table[, NCHROBS := NULL][,.(CHR,SNP,BP,A1,A2,BETA,P,MAF,place.names)]
 
 # Check alleles
 test_1 <- any(nchar(PGC_CLOZUK_python_table$A1) > 1)
 test_2 <- any(nchar(PGC_CLOZUK_python_table$A2) > 1)
+ones_to_remove <- which(is.na(PGC_CLOZUK_python_table$BP))
 
+# Produce error if it has not worked
 if (test_1 == T | test_2 == T) {
-  warning("you have some alleles which are larger than 1 bp long, check before clumping")
+  cat("Check these SNPs: ", PGC_CLOZUK_python_table$SNP[ones_to_remove])
+  stop("you have some alleles which are larger than 1 bp long, check before clumping")
 }
 
 # INSERT CONTROL SO THAT NO EXTRA COLUMNS ARE ADDED (as you've specified that you want to include all x columns)
 
 # Read in .raw file
 ## File
-file <- "./output/CLOZUK_PGC_FULL_GENOME.raw"
+file <- paste0("./output/",Validation_name,"_",Training_name,"_FULL_GENOME.raw")
 
 ## Create connection
 con <- file(description=file, open="r")
@@ -105,7 +59,6 @@ tmp <- scan(file=con, nlines=1, quiet=TRUE,what = "character")
 #n <- system(command=com, intern=TRUE)
 
 # RAW.CLOZUK.CLUMPED <- fread("./output/CLOZUK_PGC_FULL_GENOME.raw")
-ones_to_remove <- which(is.na(PGC_CLOZUK_python_table$BP))
 #testa <- PGC_CLOZUK_python_table$place.names[ones_to_remove]
 #testb <- PGC_CLOZUK_python_table[ones_to_remove]
 #testc <- PGC_CLOZUK_python_table$SNP[ones_to_remove]
@@ -114,10 +67,13 @@ ones_to_remove <- which(is.na(PGC_CLOZUK_python_table$BP))
 
 # Flow control for assigning Row ID
 setkey(PGC_CLOZUK_python_table, place.names)
-testing_col_names <- paste0(PGC_CLOZUK_python_table$SNP[place_names_for_raw],"_",PGC_CLOZUK_python_table$A1[place_names_for_raw])
+testing_col_names <- paste0(PGC_CLOZUK_python_table$SNP,"_",PGC_CLOZUK_python_table$A1)
 testing_col_names_raw <- tmp
 
+# Test if the ROW.ID's match that of the converted RAW dataset and stop if they do not
 if(all(testing_col_names == testing_col_names_raw[7:length(testing_col_names_raw)]) != T){
+  PGC_CLOZUK_python_table[, ROW.ID := c(testing_col_names)]
+  write.table(PGC_CLOZUK_python_table, file = paste0("./output/ERROR_ON_ROW_NAMES", Validation_name, "_GWAS_BGE_CLUMPED_", Training_name, "_MAF_FINAL.txt"), quote = F, row.names = T, col.names = F)
   stop("ROW.ID from input data does not match genotype data, check the column names of both .raw file and the merged file")
 }else{
   PGC_CLOZUK_python_table[, ROW.ID := c(testing_col_names)]
@@ -125,11 +81,11 @@ if(all(testing_col_names == testing_col_names_raw[7:length(testing_col_names_raw
 }
 
 # rearrange order to match Python script downstream
+# order is ROW_ID, CHR, SNP, BETA, BP, A1, P, MAF, A2
 setcolorder(PGC_CLOZUK_python_table, c(9,1,2,6,3,4,7,8,5)) 
 
 # Write MAF file to working_directory
-write.table(PGC_CLOZUK_python_table, file = paste0("./output/CLOZUK_GWAS_BGE_CLUMPED_PGC_MAF_FINAL.txt"), quote = F, row.names = T, col.names = F)
-
+write.table(PGC_CLOZUK_python_table, file = paste0("./output/", Validation_name, "_GWAS_BGE_CLUMPED_", Training_name, "_MAF_FINAL.txt"), quote = F, row.names = T, col.names = F)
 }
 
 # End timer 
