6bcb7fc113fb7db2513f026a0bf5ea5ad4361a42
John Hubert <hubertjj@cardiff.ac.uk>
2017-07-03 16:25:42 +0100

    Tested ALSPAC data in the pipeline, small changes to suit ARCCA but otherwise all appears well and have profiles for the first PGC data

--- PRS_set_whole_genome_pipeline/combining_summary_stats_tables_after_conversion_to_CHR_POS.R
+++ PRS_set_whole_genome_pipeline/combining_summary_stats_tables_after_conversion_to_CHR_POS.R
@@ -22,14 +22,14 @@ print(Chromosomes_to_split)
 
 ## Read in the latest Summary stats tables after converting to one table
 for (i in Chromosomes_to_split){
-  assign(paste0(Training_name, "_table", i), fread(paste0("./", Training_name, "_", Validation_name,"_output/", Training_name,"_table",i,"_new.txt")),envir = .GlobalEnv)
+  assign(paste0("Training_table", i), fread(paste0("./", Training_name, "_", Validation_name,"_output/", Training_name,"_table",i,"_new.txt")),envir = .GlobalEnv)
 }
 
 l = list()
 
 ## print out to one table under a common filename
 for (i in Chromosomes_to_split) {
-  l[[i]] <- eval(parse(text = paste0(Training_name,"_table",i)))
+  l[[i]] <- eval(parse(text = paste0("Training_table",i)))
 }
 combined_final_table <- rbindlist(l)
 write.table (combined_final_table, file = paste0("./", Training_name, "_", Validation_name,"_output/combined_", Training_name, "_table_with_CHR.POS_identifiers.txt"), quote = F, row.names = F)
