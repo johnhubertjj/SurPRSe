5525fd8319af32e4d549b1de42418fb01edd3e32
John Hubert <hubertjj@cardiff.ac.uk>
2017-11-28 12:09:48 +0000

    changes to argument file and others for further completion on Raven

--- PRS_set_whole_genome_pipeline/PRS_arguments_script.sh
+++ PRS_set_whole_genome_pipeline/PRS_arguments_script.sh
@@ -8,8 +8,9 @@ if [[ "$whereami" = *"raven"* ]]; then
   
   #Path_to_locations_on_the_server
   path_to_PRS_scripts="${HOME}/PhD_scripts/Schizophrenia_pipeline_scipts/"
+  
   # Re-assign to the training_set_usually_genotype and validation_full_name_without_chromosome using the sed command
-  path_to_test_dataset="/scratch/${USER}/PR54/ALSPAC_test_pipeline/ALSPAC_hrc_imputed_step3_mri_brain_measurements_only"
+  path_to_validation_dataset="/scratch/${USER}/PR54/ALSPAC_test_pipeline/ALSPAC_hrc_imputed_step3_mri_brain_measurements_only"
   path_to_training_dataset="/scratch/${USER}/PR54/ALSPAC_test_pipeline/CLOZUK_PGC2noclo.METAL.assoc.dosage"
 
   # State paths to the relevant stationary folders required for the analysis
@@ -24,17 +25,16 @@ if [[ "$whereami" = *"raven"* ]]; then
   path_to_new_fam_file="${path_to_stationary_data}CLOZUK.r7.GWAS_IDs.fam"
   path_to_gene_annotation_file="${path_to_stationary_file}NCBI37.3.gene.loc"  
   
-  # assign arguments here for now because there are so many
   # Datasets
   training_set_name="CLOZUK_PGC2noclo"
   validation_set_name="ALSPAC_brain_measurements_only" 
-  training_set_original_filename="CLOZUK_PGC2noclo.METAL.assoc.dosage"
-  validation_set_full_name_without_chromosome="ALSPAC_hrc_imputed_step3_mri_brain_measurements_only"
-  
+ 
   # DO NOT ALTER!!!
   validation_set_usually_genotype="${validation_set_full_name_without_chromosome}_chr${chromosome_number}"
   validation_set_usually_genotype_serial="${validation_set_full_name_without_chromosome}_chr"
   training_set_usually_summary="${training_set_name}_table${chromosome_number}"
+  training_set_original_filename=`echo "${path_to_training_dataset}" | sed 's:.*/::'`
+  validation_set_full_name_without_chromosome=`echo "${path_to_validation_dataset}" | sed 's:.*/::'`
   
   #Pathway datasets
   Pathway_filename="Pocklington2015_134sets_LoFi.txt"
